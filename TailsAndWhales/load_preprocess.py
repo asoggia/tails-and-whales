0309fd98b3f09f1be44db69890d92110fdb15f7c
anthony crepet <anthony.crepet@gmail.com>
2022-04-11 09:16:54 +0200

    travail du WE anthony

--- TailsAndWhales/load_preprocess.py
+++ TailsAndWhales/load_preprocess.py
@@ -9,15 +9,7 @@ from tensorflow.keras.utils import to_categorical
 from google.colab import auth
 
 
-def create_X_y(number_of_photos):
-    # load and make changes to csv
-    auth.authenticate_user()
-    echo "deb http://packages.cloud.google.com/apt gcsfuse-bionic main" > /etc/apt/sources.list.d/gcsfuse.list
-    curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add -
-    apt -qq update
-    apt -qq install gcsfuse
-    mkdir tails-and-whales
-    gcsfuse tails-and-whales tails-and-whales
+"""def create_X_y(number_of_photos):
     path = 'tails-and-whales/train_kaggle.csv'
     data = pd.read_csv(path)
     data['speciesv2'] = data['species']
@@ -61,6 +53,60 @@ def create_X_y(number_of_photos):
     num_classes = 3
     y = to_categorical(data['class'][:number_of_photos], num_classes=num_classes)
     with open("data/y.pkl", "wb") as fp:   #Pickling
+        pickle.dump(y, fp)
+
+    print("X and y are saved ! Well Done !")"""
+
+
+def create_X_y(csv_path = '../raw_data/train_kaggle.csv'): #number of photos could be a parameter
+    # load and make changes to csv
+    data = pd.read_csv(csv_path)
+    data['speciesv2'] = data['species']
+    data['speciesv2'].loc[data['speciesv2'].str.contains('dolphin')] = 'dolphin'
+    data['speciesv2'].loc[data['speciesv2'].str.contains('dolpin')] = 'dolphin'
+    data['speciesv2'].loc[data['speciesv2'].str.contains('whale')] = 'whale'
+    data = data[data['speciesv2'] != 'globis']
+    # create and save X in raw-data/X.pkl
+    path = "../raw_data/train_images/"
+    images_list = list(data['image'])
+
+    # This block code includes a resizing step
+    loaded_images = []
+    a = 1
+    for index, image in enumerate(images_list):   #number of photos [:number_of_photos]
+        print(f" processing photo number {a}")
+
+        # resizing image
+        img = mpimg.imread(path + image)
+        img = cv2.resize(img, dsize=(256, 256), interpolation= cv2.INTER_LINEAR)
+        a +=1
+
+        # reshaphe photo in 3D dimension
+        if len(img.shape) != 3:
+            #list_index_bw.append(index)
+            #list_len.append(len(img.shape))
+            img = np.stack((img,)*3, axis=-1)
+
+        loaded_images.append(np.array(img))
+
+    # convert to 3D array
+    X = np.array(loaded_images)
+    print(X.shape)
+    # get gcp bucket path
+
+    preprocessed_data_path = "../raw_data/preprocessed_data/"
+    # save preprocessed images to GCP bucket
+    with open(os.path.join(preprocessed_data_path,"X.pkl"), "wb") as fp:   #Pickling
+        print(fp)
+        pickle.dump(X, fp)
+
+    # create and save y in raw_data/y.pkl
+    classes = {'whale':0, 'dolphin':1, 'beluga':2}
+    #Add a new column 'class' on data_sample df
+    data['class'] = data['speciesv2'].map(classes)
+    num_classes = 3
+    y = to_categorical(data['class'], num_classes=num_classes) #number of photos [:number_of_photos]
+    with open(os.path.join(preprocessed_data_path,"y.pkl"), "wb") as fp:   #Pickling
         pickle.dump(y, fp)
 
     print("X and y are saved ! Well Done !")

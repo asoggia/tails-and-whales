6e80873577cd5900dc1c3843aca200ce310e0997
anthony crepet <anthony.crepet@gmail.com>
2022-05-12 11:12:36 +0200

    update

--- Dockerfile
+++ Dockerfile
@@ -1,10 +1,14 @@
-FROM python:3.8.6-buster
+FROM python:3.8.12-buster
 
 COPY api /api
 COPY TailsAndWhales /TailsAndWhales
-COPY "model" / "model" # A remplir précisément.
+COPY image /image
+COPY model_7 /model_7
 COPY requirements.txt /requirements.txt
 
+RUN apt-get update && apt-get install -y python3-opencv
+RUN pip install opencv-python
+RUN pip install --upgrade pip
 RUN pip install -r requirements.txt
 
 CMD uvicorn api.fast:app --host 0.0.0.0 --port $PORT
--- api/fast.py
+++ api/fast.py
@@ -1,5 +1,11 @@
 from fastapi import FastAPI
+from fastapi import Request
 from fastapi.middleware.cors import CORSMiddleware
+from tensorflow.keras.models import load_model
+from tensorflow.keras.applications.vgg16 import preprocess_input
+import cv2
+import matplotlib.image as mpimg
+import numpy as np
 
 app = FastAPI()
 
@@ -13,8 +19,20 @@ app.add_middleware(
 
 @app.get("/")
 def index():
-    return {"greeting": "Hello world"}
+    return {"greeting": "Hello Whales !"}
 
-@app.get("/predict")
-def predict():
-    pass # A compléter // Doit retourner la lsite des probabilités
+@app.get("/predict/{name_img}")
+async def predict(name_img):
+    img = mpimg.imread("image/" + name_img)  # changer le path !!!
+    img = cv2.resize(img, dsize=(64, 64), interpolation= cv2.INTER_LINEAR)
+
+    X = np.array(img)
+    X = preprocess_input(X)
+    X = np.expand_dims(X, axis=0)
+
+    reconstructed_model = load_model('model_7/') #attention changer le path
+    prediction = reconstructed_model.predict(X)
+
+    prediction = [float(i) for i in prediction[0]]
+    reponse = {"prediction": prediction}
+    return reponse
